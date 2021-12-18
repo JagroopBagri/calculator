7760be1b40b11235b8337dc5691f2da94d6eade9
Jagroop <jagroopbagri5@gmail.com>
2021-12-17 20:53:27 -0800

    Added index.html, main.js, and styles.css file with basic calculator outline

---
+++ main.js
@@ -0,0 +1,66 @@
+
+// Adding, subtracting, multiplying, and dividing with arrays
+/*
+function addArr(array){
+    return array.reduce(function(sum, numbers){
+            sum += numbers
+            return sum;
+    }, 0);
+}
+
+function subtractArr(array){
+    return array.reduce(function(difference, numbers){
+            difference -= numbers
+            return difference;
+    });
+}
+
+function multiplyArr(array){
+    return array.reduce(function(product, numbers){
+            product *= numbers
+            return product;
+    });
+}
+
+function divideArr(array){
+    return array.reduce(function(quotient, numbers){
+        if(numbers === 0){
+            return 'undefined';
+        }
+        else{
+            quotient /= numbers
+            return quotient;
+        }
+    });
+}
+*/
+
+// Functions for adding, subtracting, multiplying, and dividing two numbers
+
+function add(x,y){
+    return x + y;
+}
+function subtract(x,y){
+    return x - y;
+}
+function multiply(x,y){
+    return x * y;
+}
+function divide(x,y){
+    return (y === 0 ? 'undefined' : x/y);
+}
+// Function that uses one of the above functions based on which operand is input as a parameter
+function operate(operator, num1, num2){
+    if(operator === '+'){
+        return add(num1, num2)
+    }
+    else if(operator === '-'){
+        return subtract(num1, num2)
+    }
+    else if(operator === '*'){
+        return multiply(num1, num2)
+    }
+    else if(operator === '/'){
+        return divide(num1, num2)
+    }
+}
